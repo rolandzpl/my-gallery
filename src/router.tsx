e269ac10699549c4b6e8bd68067f093da1901f19
Roland Żerek <rolandz@poczta.fm>
2025-08-24 22:22:07 +0200

    Implemented image converter - added route and menu item

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import AdminPage from "./Admin/pages";
 import Layout from "./Layout";
 import { GalleryCreate, GalleryIndex, GalleryPage } from "./Gallery/pages";
 import { HomePage } from "./Home/pages";
+import { ImageConverterPage } from "./ImageConverter";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,14 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <AdminPage />,
+          }
+        ]
+      },
+      {
+        path: "image-converter",
+        children: [
+          {
+            index: true, element: <ImageConverterPage />
           }
         ]
       },
@@ -41,6 +50,7 @@ const router = createBrowserRouter([
     ],
     element: (<Layout menuItems={[
       { key: 'home', label: 'Home', url: '/' },
+      { key: 'image-converter', label: 'Image Converter', url: '/image-converter' },
       { key: 'galleries', label: 'Galleries', url: '/gallery' },
       { key: 'about', label: 'About', url: '/about' },
       { key: 'contact', label: 'Contact', url: '/contact' }
